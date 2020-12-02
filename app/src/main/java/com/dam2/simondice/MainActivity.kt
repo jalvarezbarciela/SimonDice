139595b33b2901c3e10637668577a085620ced22
jalvarezbarciela <jalvarezbarciela@danielcastelao.org>
2020-12-02 14:12:22 +0100

    Simon dice(final)

--- app/src/main/java/com/dam2/simondice/MainActivity.kt
+++ app/src/main/java/com/dam2/simondice/MainActivity.kt
@@ -2,23 +2,74 @@ package com.dam2.simondice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.widget.Button
 import android.widget.Toast
+
+
+
+import androidx.lifecycle.Observer
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var sec: MutableList<Int> = mutableListOf<Int>()
-        var user_sec: MutableList<Int> = mutableListOf<Int>()
-        var finished  = false
-        val button_red : Button = findViewById(R.id.rojo)
-        val button_yellow : Button = findViewById(R.id.amarillo)
-        val button_blue : Button = findViewById(R.id.azul)
-        val button_green : Button = findViewById(R.id.verde)
-        val button_start : Button = findViewById(R.id.start)
-
+        var Estado : Boolean
+        val butonrojo : Button = findViewById(R.id.rojo)
+        val botonamarillo : Button = findViewById(R.id.amarillo)
+        val botonazul : Button = findViewById(R.id.azul)
+        val botonverde : Button = findViewById(R.id.verde)
+        val start : Button = findViewById(R.id.start)
+        val check : Button = findViewById(R.id.check)
+        val Btcolor = listOf(butonrojo,botonverde,botonamarillo,botonazul)
         val toast = Toast.makeText(applicationContext,"Juego terminado", Toast.LENGTH_SHORT)
         val toast2 = Toast.makeText(applicationContext,"Inicio", Toast.LENGTH_SHORT)
+
+        val ModeloJuego by viewModels<liveData>()
+
+        ModeloJuego.gameState.observe(this, Observer{
+                gs -> Estado = gs
+            if(Estado) {
+                butonrojo.setEnabled(false)
+                botonamarillo.setEnabled(false)
+                botonazul.setEnabled(false)
+                botonverde.setEnabled(false)
+                check.setEnabled(false)
+            }else{
+                butonrojo.setEnabled(true)
+                botonamarillo.setEnabled(true)
+                botonazul.setEnabled(true)
+                botonverde.setEnabled(true)
+                check.setEnabled(true)
+            }
+        })
+
+        ModeloJuego.secuenciaJ.observe(this, Observer{
+            ModeloJuego.showSec(Btcolor)
+        })
+
+        start.setOnClickListener{
+            toast2.show()
+            ModeloJuego.init_game()
+        }
+
+        check.setOnClickListener{
+            if(!ModeloJuego.checkSec()){
+                toast.show()
+            }
+        }
+        butonrojo.setOnClickListener{
+            ModeloJuego.addUserSec(1)
+        }
+        botonverde.setOnClickListener{
+            ModeloJuego.addUserSec(2)
+        }
+        botonamarillo.setOnClickListener{
+            ModeloJuego.addUserSec(3)
+        }
+        botonazul.setOnClickListener{
+            ModeloJuego.addUserSec(4)
+        }
     }
 }
